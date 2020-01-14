860606901643128f5692a5cd458277f21f8eb02f
Brendan Kelley <brkelley@pendo.io>
2020-01-14 13:44:12 -0500

    removed a thing?

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -18,8 +18,8 @@ if (token) {
 const store = createStore(
     rootReducer,
     compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        applyMiddleware(thunk)
+        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 );
 
